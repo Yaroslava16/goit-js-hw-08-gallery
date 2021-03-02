57849f6480f167d62f65f570b2423b034f875b7e
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2021-03-03 00:04:41 +0200

    Исправляет добавленную разметку изображений и начинает работу над модалкой

--- js/index.js
+++ js/index.js
@@ -2,13 +2,26 @@ import galleryItems from './data/gallery-items.js';
 
 const galleryListEl = document.querySelector('.js-gallery');
 
+const lightbox = document.querySelector('.js-lightbox');
+
 const makeAnImageGallery = galleryItems
   .map(
-    ({ preview, description }) => `
-  <li>
-    <img src = "${preview}" alt = "${description}">
-  </li>`,
+    ({ preview, original, description }) => `
+<li class = "gallery__item"><a class="gallery__link" href="${original}"> <image class = "gallery__image" src = "${preview}" data-source="${original}" alt = "${description}"></a></li>`,
   )
   .join('');
 
-galleryListEl.insertAdjacentHTML('afterend', makeAnImageGallery);
+galleryListEl.insertAdjacentHTML('beforeend', makeAnImageGallery);
+
+galleryListEl.addEventListener('click', onGalleriesImageClick);
+
+function onGalleriesImageClick(evt) {
+  if (!evt.target.classList.contains('.gallery__image')) {
+    return;
+  }
+
+  const selectedEl = evt.target;
+  const parentCard = selectedEl.closest('.js-gallery');
+
+  lightbox.classList.add('is-open');
+}
