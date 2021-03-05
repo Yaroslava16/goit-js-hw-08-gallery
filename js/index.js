d5b59a0035267fdd2a2b89e3bf0a38e8479cddb7
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2021-03-05 15:11:41 +0200

    Пытается сделать закрытие модалки по кнопке
    Безуспешно

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ import galleryItems from './data/gallery-items.js';
 const galleryListEl = document.querySelector('.js-gallery');
 
 const lightbox = document.querySelector('.js-lightbox');
+const closeModalBtn = document.querySelector('[data-action="close-lightbox"]');
 
 const makeAnImageGallery = galleryItems
   .map(
@@ -15,6 +16,8 @@ galleryListEl.insertAdjacentHTML('beforeend', makeAnImageGallery);
 
 galleryListEl.addEventListener('click', onGalleriesImageClick);
 
+closeModalBtn.addEventListener('click', onCloseModalbtn);
+
 function onGalleriesImageClick(evt) {
   if (!evt.target.classList.contains('.gallery__image')) {
     return;
@@ -23,5 +26,9 @@ function onGalleriesImageClick(evt) {
   const selectedEl = evt.target;
   const parentCard = selectedEl.closest('.js-gallery');
 
-  lightbox.classList.add('is-open');
+  document.lightbox.classList.add('is-open');
+}
+
+function onCloseModalbtn(evt) {
+  document.lightbox.classList.remove('is-open');
 }
