85ce08180e5b8d6eaa3c7c7c3f7827cda4dbb708
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2021-03-09 17:58:20 +0200

    Пытается сделать пролистывание галереи клавишами влево, вправо

--- js/index.js
+++ js/index.js
@@ -27,12 +27,26 @@ overleyModal.addEventListener('click', onCloseModal);
 
 function onGalleriesImageClick(evt) {
   evt.preventDefault();
+  modalImg.setAttribute('data-index');
 
   const selectedEl = evt.target;
   const selectedElData = selectedEl.dataset.source;
 
   if (selectedEl) {
     window.addEventListener('keydown', onEscKeyPress);
+
+    window.addEventListener('keydown', e => {
+      if (e.code === 'ArrowLeft') {
+        onArrowLeft();
+      }
+
+      if (e.code === 'ArrowRight') {
+        onArrowRight();
+      }
+    });
+
+    window.addEventListener('keydown', onArrowRight);
+
     lightbox.classList.add('is-open');
     modalImg.src = selectedElData;
     modalImg.alt = selectedEl.alt;
@@ -52,4 +66,27 @@ function onEscKeyPress(evt) {
   if (evt.code === 'Escape') {
     onCloseModal();
   }
+}
+function setNewSrc(step, index) {
+  modalImg.dataset.index = `${index + step}`;
+  modalImg.src = galleryItems[index + step].original;
+}
+
+function onArrowLeft(evt) {
+  let index = Number(modalImg.dataset.index);
+  if (index === 0) {
+    setNewSrc(0, galleryItems.length - 1);
+    return;
+  }
+  setNewSrc(-1, index);
+}
+
+function onArrowRight(evt) {
+  let index = Number(modalImg.dataset.index);
+  if (index === galleryItems.length - 1) {
+    setNewSrc(0, 0);
+    return;
+  }
+  console.log(index);
+  setNewSrc(1, index);
 }
